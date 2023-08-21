f6973685bdd9a2e5baf5de4a27591e1febad8cf0
zhengsi <zhengsi@360.cn>
2023-08-21 18:51:36 +0800

    去掉md

--- app/help/page.js
+++ app/help/page.js
@@ -1,15 +1,9 @@
-import { MDXRemote } from 'next-mdx-remote/rsc'
-import path from 'path'
-import fs from 'fs'
+export default function Page() {
 
-export default async function Page() {
-  const filePath = path.join(process.cwd(), '/doc/help.md');
-  const fileContent = fs.readFileSync(filePath, 'utf8');
-  const markdown = fileContent.toString();
   return (
     <div className="container mx-auto py-10">
       <article className="prose lg:prose-xl">
-        <MDXRemote source={markdown} />
+
       </article>
     </div>
   )
