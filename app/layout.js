e4894b6374b19d41a4998d53044052d852da08be
zhengsi <zhengsi@360.cn>
2023-09-08 09:55:43 +0800

    clarity

--- app/layout.js
+++ app/layout.js
@@ -13,7 +13,6 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body>{children}</body>
       <Script
-        id="show-banner"
         dangerouslySetInnerHTML={{
           __html: `var _hmt = _hmt || [];
 (function() {
@@ -21,7 +20,12 @@ export default function RootLayout({ children }) {
   hm.src = "https://hm.baidu.com/hm.js?ce0f4be946c87b5effa495bd3a79ffc6";
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
-})();`,
+})();
+(function(c,l,a,r,i,t,y){
+    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+})(window, document, "clarity", "script", "irpu3fcu1e");`,
         }}
       />
     </html>
