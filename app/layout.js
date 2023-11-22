20448b6203d4af18835e6c1158e34f9233ee18c7
j20cc <29476413+j20cc@users.noreply.github.com>
2023-11-22 10:53:13 +0800

    移除 clarity

--- app/layout.js
+++ app/layout.js
@@ -22,11 +22,6 @@ export default function RootLayout({ children }) {
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
-(function(c,l,a,r,i,t,y){
-    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-})(window, document, "clarity", "script", "irpu3fcu1e");`,
         }}
       />
     </html>
